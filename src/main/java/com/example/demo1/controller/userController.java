fb2721338a0abb87862132be2a923263567c8175
JeremyHua <JeremyHua@foxmail.com>
2022-03-07 23:05:33 +0800

    修改前端表格交互

--- src/main/java/com/example/demo1/controller/userController.java
+++ src/main/java/com/example/demo1/controller/userController.java
@@ -10,26 +10,48 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.example.demo1.service.*;
 
 import java.sql.SQLException;
+import java.util.Arrays;
 
 @Controller
 public class userController {
     @RequestMapping(path="/user",method = RequestMethod.GET)
     public String user_html(){
 
-        return "forward:test3.html";
+        return "forward:table_test_admin.html";
     }
 
-    @RequestMapping(path = "/queryDB",method = RequestMethod.POST)
+    @RequestMapping(path = "/user",method = RequestMethod.POST)
     @ResponseBody
-    public JSONArray queryDB(@RequestBody(required=false) String str) throws SQLException {
+    public JSONArray queryDB(@RequestBody(required=false) JSONObject json_search) throws SQLException {
 
         JSONArray jsonArray = new JSONArray();
-        JSONObject tempJSON = new JSONObject();
-        tempJSON.put("id","0");
-        tempJSON.put("kind","0");
-        parseToSQL temp = new parseToSQL();
-        jsonArray = temp.parse0(tempJSON);
-        System.out.println(jsonArray);
+        System.out.println(json_search);
+//        parseToSQL temp = new parseToSQL();
+//        jsonArray = temp.parse0(json_search);  //查阅所有书籍
+//        System.out.println(jsonArray);
+
+        JSONObject json1=new JSONObject();
+        json1.put("book_id","222");
+        json1.put("book_name","333");
+        json1.put("author","4444");
+        json1.put("collection_number","5555");
+        json1.put("existing_number","6666");
+        json1.put("price","5555");
+        json1.put("publisher","66666");
+        json1.put("introduction","77777");
+        jsonArray.add(json1);
+
+        JSONObject json2=new JSONObject();
+        json2.put("book_id","0000222");
+        json2.put("book_name","0000333");
+        json2.put("author","00004444");
+        json2.put("collection_number","00005555");
+        json2.put("existing_number","00006666");
+        json2.put("price","0005555");
+        json2.put("publisher","00066666");
+        json2.put("introduction","0077777");
+
+        jsonArray.add(json2);
 
         return jsonArray;
 
